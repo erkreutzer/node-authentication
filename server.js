c4f617fc1a3515f3d9850ed4c9f3ec4dff2143a9
Erich Kreutzer <erich.kreutzer@gmail.com>
2014-04-27 16:45:04 -0700

    adding local authentication

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+
+var logger   = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash 	 = require('connect-flash');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+app.use(logger());
+app.use(cookieParser());
+app.use(bodyParser());
+app.set('view engine', 'ejs');
+app.use(session({secret: 'ilovescotchscotchyscotchscotch'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
